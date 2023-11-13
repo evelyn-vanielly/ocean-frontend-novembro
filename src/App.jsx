fdc177c9868c247cf9cae5d1f2cc13687367ed91
Evelyn Vanielly <67793057+evelyn-vanielly@users.noreply.github.com>
2023-11-13 23:49:56 +0000

    Commit de Tags

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ return (
     <>
     <div className="card">
     <h2>Rick Sanchez</h2>
+   <div className='Tags'> 
+    <div className='Tag'> Status: Vivo</div>
+    <div className='Tag'> Espécie: Humana</div>
+    <div className='Tag'> Origem: Terra C-137</div>
+   </div>
     <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />  
     </div> 
 
